e946271102f060fc3bae91d6b9ebdee546f3ef57
31320600 <31320600@093133-P31S302.macklabs.br>
2016-04-20 12:00:11 -0300

    Adição de bibliotecas 2

--- test/org/fpij/jitakyoei/model/ModelIntegrationTests.java
+++ test/org/fpij/jitakyoei/model/ModelIntegrationTests.java
@@ -19,47 +19,47 @@ public class ModelIntegrationTests {
 	private static Filiado filiadoAluno;
 	private static Filiado filiadoProf;
 	private static Professor professor;
-	
-	@BeforeClass
-	public static void setUp(){
-		DatabaseManager.setEnviroment(DatabaseManager.TEST);
-		filiadoAluno = new Filiado();
-		filiadoAluno.setNome("Aécio");
-		filiadoAluno.setCpf("036.464.453-27");
-		filiadoAluno.setDataNascimento(new Date());
-		filiadoAluno.setDataCadastro(new Date());
-		filiadoAluno.setId(1332L);
-		filiadoAluno.setRg(new Rg("5026762-0", "SSP-PI"));
-		
-		endereco = new Endereco();
-		endereco.setBairro("Dirceu");
-		endereco.setCep("64078-213");
-		endereco.setCidade("Teresina");
-		endereco.setEstado("PI");
-		endereco.setRua("Rua Des. Berilo Mota");
-		
-		filiadoProf = new Filiado();
-		filiadoProf.setNome("Professor");
-		filiadoProf.setCpf("036.464.453-27");
-		filiadoProf.setDataNascimento(new Date());
-		filiadoProf.setDataCadastro(new Date());
-		filiadoProf.setId(3332L);
-		filiadoProf.setEndereco(endereco);
-		
-		professor = new Professor();
-		professor.setFiliado(filiadoProf);
-		
-		entidade = new Entidade();
-		entidade.setEndereco(endereco);
-		entidade.setNome("Academia 1");
-		entidade.setTelefone1("(086)1234-5432");
-		
-		aluno = new Aluno();
-		aluno.setFiliado(filiadoAluno);
-		aluno.setProfessor(professor);
-		aluno.setEntidade(entidade);
-		
-//		alunoDao = new DAOImpl<Aluno>(Aluno.class);
-	}
+//	
+//	@BeforeClass
+//	public static void setUp(){
+//		DatabaseManager.setEnviroment(DatabaseManager.TEST);
+//		filiadoAluno = new Filiado();
+//		filiadoAluno.setNome("Aécio");
+//		filiadoAluno.setCpf("036.464.453-27");
+//		filiadoAluno.setDataNascimento(new Date());
+//		filiadoAluno.setDataCadastro(new Date());
+//		filiadoAluno.setId(1332L);
+//		filiadoAluno.setRg(new Rg("5026762-0", "SSP-PI"));
+//		
+//		endereco = new Endereco();
+//		endereco.setBairro("Dirceu");
+//		endereco.setCep("64078-213");
+//		endereco.setCidade("Teresina");
+//		endereco.setEstado("PI");
+//		endereco.setRua("Rua Des. Berilo Mota");
+//		
+//		filiadoProf = new Filiado();
+//		filiadoProf.setNome("Professor");
+//		filiadoProf.setCpf("036.464.453-27");
+//		filiadoProf.setDataNascimento(new Date());
+//		filiadoProf.setDataCadastro(new Date());
+//		filiadoProf.setId(3332L);
+//		filiadoProf.setEndereco(endereco);
+//		
+//		professor = new Professor();
+//		professor.setFiliado(filiadoProf);
+//		
+//		entidade = new Entidade();
+//		entidade.setEndereco(endereco);
+//		entidade.setNome("Academia 1");
+//		entidade.setTelefone1("(086)1234-5432");
+//		
+//		aluno = new Aluno();
+//		aluno.setFiliado(filiadoAluno);
+//		aluno.setProfessor(professor);
+//		aluno.setEntidade(entidade);
+//		
+////		alunoDao = new DAOImpl<Aluno>(Aluno.class);
+//	}
 	
 }
